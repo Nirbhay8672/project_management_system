4e9bd923893920ae5ce08649d505aa1b6976e9f6
Nirbhay8672 <71960388+Nirbhay8672@users.noreply.github.com>
2024-04-05 13:33:25 +0530

    Installation inertia and vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+require("./bootstrap");
+
+import Vue from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+
+createInertiaApp({
+    resolve: (name) => require(`./pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        Vue.use(plugin);
+
+        new Vue({
+            render: (h) => h(App, props),
+        }).$mount(el);
+    },
+});
---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hello this is first page .</h1>
+</template>
